b27ccd5fad5568c321be3634d244e3a10d8fb17c
RUTUJ PURANIK <71043897+rp185335@users.noreply.github.com>
2021-04-21 17:34:19 -0500

    New project created to demonstrate web api code

---
+++ Services/IProductsService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebApiCRUD.Services
+{
+    public interface IProductsService
+    {
+        public List<Product> GetProducts();
+
+        public Product AddProduct(Product productItem);
+
+        public Product UpdateProduct(string id, Product productItem);
+
+        public string DeleteProduct(string id);
+
+
+    }
+}
